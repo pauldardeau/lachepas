7738c8129cfea596ebfce5c150d42f08ad692c6a
Paul Dardeau <pauldardeau@gmail.com>
2025-11-16 20:28:59 -0600

    fix botched removal of extraneous spaces

--- src/AESEncryption.cpp
+++ src/AESEncryption.cpp
@@ -1,247 +1,247 @@
+// AESEncryption.cpp
 
+#include <string.h>
+#include <openssl/evp.h>
 
+#include "AESEncryption.h"
+#include "Data.h"
 
+using namespace std;
 
+static const string EMPTY = "";
 
+using namespace lachepas;
 
+//*****************************************************************************
 
+bool AESEncryption::decrypt(const Data& key,
+                            const Data& iv,
+                            const Data& cipherText,
+                            string& plainText) {
+   EVP_CIPHER_CTX ctx;
+   int rc;
+   bool success = false;
 
+   /*
+
+   ::EVP_CIPHER_CTX_init(&ctx);
 
+   rc = ::EVP_DecryptInit(&ctx, EVP_aes_256_cbc(), key.cdata(), iv.cdata());
 
+   if (1 == rc) {
+      const size_t cipherLen = cipherText.size();
+      size_t remainingBytes = cipherLen;
 
+      char *decryptedData = new char[cipherText.size()+1];
+      ::memset(decryptedData, 0, cipherText.size()+1);
+      int decryptedLength = 0;
 
+      Data out(0, 16);
+      int outl = 0;
 
+      rc = ::EVP_DecryptUpdate(ctx,
+                               decryptedData,
+                               &decryptedLength,
+                               in,
+                               inl);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      if (1 == rc) {
+         remainingBytes -= outl;
+      }
+
+      rc = ::EVP_DecryptFinal_ex(ctx, outm, &outl);
+      if (1 == rc) {
+         success = true;
+      }
+   }
+
+   ::EVP_CIPHER_CTX_cleanup(&ctx);
+   */
+
+   return success;
+}
+
+//*****************************************************************************
+
+string AESEncryption::decrypt(const Data& key,
+                                   const Data& iv,
+                                   const Data& cipherText) {
+   string plainText;
+   if (decrypt(key, iv, cipherText, plainText)) {
+      return plainText;
+   } else {
+      return EMPTY;
+   }
+}
+
+//*****************************************************************************
+
+/*
+Data AESEncryption::encrypt(const Data& key,
+                            const Data& iv,
+                            const Data& plainText) {
+
+}
+*/
+//*****************************************************************************
+
+unsigned char* AESEncryption::encrypt(const Data& key,
+                                      const Data& iv,
+                                      const string& plainText) {
+   EVP_CIPHER_CTX ctx;
+   int rc;
+   bool success = false;
+
+   ::EVP_CIPHER_CTX_init(&ctx);
+
+   // set padding to true/on
+   rc = EVP_CIPHER_CTX_set_padding(&ctx, 1);
+
+   rc = EVP_CIPHER_CTX_set_key_length(&ctx, 32);
+
+   /*
+   int EVP_EncryptInit_ex(EVP_CIPHER_CTX *ctx,
+                          const EVP_CIPHER *type,
+                          ENGINE *impl,
+                          unsigned char *key,
+                          unsigned char *iv);
+
+   EVP_EncryptInit_ex() sets up cipher context ctx for encryption
+   with cipher type from ENGINE impl. ctx must be initialized before
+   calling this function. type is normally supplied by a function
+   such as EVP_des_cbc(). If impl is NULL then the default implementation
+   is used. key is the symmetric key to use and iv is the IV to use (if
+   necessary), the actual number of bytes used for the key and IV depends
+   on the cipher. It is possible to set all parameters to NULL except type
+   in an initial call and supply the remaining parameters in subsequent
+   calls, all of which have type set to NULL. This is done when the default
+   cipher parameters are not appropriate.
+   */
+   rc = ::EVP_EncryptInit(&ctx, EVP_aes_256_cbc(), key.cdata(), iv.cdata());
+
+   if (1 == rc) {
+
+      //Data in(0, 16);
+      //Data out(0, 32); /* at least one block longer than in[] */
+
+      const unsigned blockSize = 16;  // 128-bit block size
+
+      unsigned outLen = plainText.size();
+      outLen += blockSize;
+
+      unsigned char *outp = (unsigned char*) malloc(outLen);
+
+      int outl = outLen;
+      int outlen2;
+
+      /*
+      int EVP_EncryptUpdate(EVP_CIPHER_CTX *ctx,
+                            unsigned char *out,
+                            int *outl,
+                            unsigned char *in,
+                            int inl);
+
+      EVP_EncryptUpdate() encrypts inl bytes from the buffer in and
+      writes the encrypted version to out. This function can be called
+      multiple times to encrypt successive blocks of data. The amount
+      of data written depends on the block alignment of the encrypted
+      data: as a result the amount of data written may be anything from
+      zero bytes to (inl + cipher_block_size - 1) so outl should contain
+      sufficient room. The actual number of bytes written is placed in outl.
+      */
+      rc = ::EVP_EncryptUpdate(&ctx,
+                               outp,
+                               &outl,
+                               (unsigned char*)plainText.data(),
+                               plainText.size());
+
+      if (1 == rc) {
+
+         outp += outl;
+         outl = outLen - outl;
+
+         /*
+         int EVP_EncryptFinal_ex(EVP_CIPHER_CTX *ctx,
+                                 unsigned char *out,
+                                 int *outl);
+
+         If padding is enabled (the default) then EVP_EncryptFinal_ex()
+         encrypts the ``final'' data, that is any data that remains in a
+         partial block. It uses standard block padding (aka PKCS padding).
+         The encrypted final data is written to out which should have
+         sufficient space for one cipher block. The number of bytes written
+         is placed in outl. After this function is called the encryption
+         operation is finished and no further calls to EVP_EncryptUpdate()
+         should be made.
+
+         If padding is disabled then EVP_EncryptFinal_ex() will not encrypt
+         any more data and it will return an error if any data remains in a
+         partial block: that is if the total data length is not a multiple
+         of the block size.
+         */
+         rc = ::EVP_EncryptFinal_ex(&ctx, outp, &outl);
+
+         if (1 == rc) {
+            return outp;
+            //success = true;
+            //cipherText.append(out);
+         } else {
+            ::printf("error: EVP_EncryptFinal_ex failed\n");
+         }
+      } else {
+         ::printf("error: EVP_EncryptUpdate failed\n");
+      }
+
+      ::free(outp);
+   } else {
+      ::printf("error: EVP_EncryptInit_ex failed\n");
+   }
+
+   ::EVP_CIPHER_CTX_cleanup(&ctx);
+
+   //return success;
+   return nullptr;
+}
+
+//*****************************************************************************
+/*
+Data AESEncryption::encrypt(const Data& key,
+                            const Data& iv,
+                            const string& plainText) {
+   Data cipherText;
+   if (encrypt(key, iv, plainText, cipherText)) {
+      return cipherText;
+   } else {
+      return Data();
+   }
+}
+*/
+//*****************************************************************************
+
+void AESEncryption::test() {
+   unsigned char key[32] = {0};
+   unsigned char iv[16] = {0};
+   unsigned char in[16] = {0};
+   unsigned char out[32]; /* at least one block longer than in[] */
+   int outlen1;
+   int outlen2;
+   int rc;
+
+   ::memset(out, 0, 32);
+
+   //                  0        1         2         3
+   //                  12345678901234567890123456789012
+   ::strcpy((char*)key, "sksiewk48wjaKkl2#2ln120,4os@+x<}");
+
+   //                 0        1
+   //                 1234567890123456
+   ::strcpy((char*)iv, "*dlk3EosMdi50;x");
+
+   //                 0        1
+   //                 1234567890123456
+   ::strcpy((char*)in, "Now is the time");
+}
+
+//*****************************************************************************
 
 
--- src/GFSMessage.cpp
+++ src/GFSMessage.cpp
@@ -1,352 +1,352 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Copyright Paul Dardeau, 2016
+// GFSMessage.cpp
+
+#include <stdlib.h>
+
+#include "GFSMessage.h"
+#include "Message.h"
+#include "StringTokenizer.h"
+
+using namespace std;
+using namespace lachepas;
+
+static const string LIST_DELIMITER         = "|";
+
+static const string KEY_PREFIX             = "gfs_";
+
+static const string KEY_RC                 = KEY_PREFIX + "rc";
+static const string KEY_ERROR              = KEY_PREFIX + "error";
+
+static const string KEY_DEVICE             = KEY_PREFIX + "device";
+static const string KEY_DEVICE_LIST        = KEY_PREFIX + "deviceList";
+static const string KEY_DIR                = KEY_PREFIX + "dir";
+static const string KEY_DIR_LIST           = KEY_PREFIX + "dirList";
+static const string KEY_FILE               = KEY_PREFIX + "file";
+static const string KEY_FILE_LIST          = KEY_PREFIX + "fileList";
+static const string KEY_ORIGIN_FS          = KEY_PREFIX + "origin_fs";
+static const string KEY_STORED_FS          = KEY_PREFIX + "stored_fs";
+static const string KEY_UNIQUE_IDENTIFIER  = KEY_PREFIX + "unique_id";
+
+static const string VALUE_TRUE             = "true";
+static const string VALUE_FALSE            = "false";
+
+//******************************************************************************
+
+void GFSMessage::setKeyValue(tonnerre::Message& message,
+                             const string& key,
+                             const string& value) {
+   message.setHeader(key, value);
+}
+
+//******************************************************************************
+
+bool GFSMessage::hasKey(const tonnerre::Message& message,
+                        const string& key) {
+   return message.hasHeader(key);
+}
+
+//******************************************************************************
+
+const string& GFSMessage::getKeyValue(const tonnerre::Message& message,
+                                      const string& key) {
+   return message.getHeader(key);
+}
+
+//******************************************************************************
+
+void GFSMessage::setRC(tonnerre::Message& message, bool rc) {
+   if (rc) {
+      GFSMessage::setKeyValue(message, KEY_RC, VALUE_TRUE);
+   } else {
+      GFSMessage::setKeyValue(message, KEY_RC, VALUE_FALSE);
+   }
+}
+
+//******************************************************************************
+
+bool GFSMessage::getRC(const tonnerre::Message& message) {
+   bool rc = false;
+   if (GFSMessage::hasKey(message, KEY_RC)) {
+      const string& rcValue = GFSMessage::getKeyValue(message, KEY_RC);
+      if (rcValue == VALUE_TRUE) {
+         rc = true;
+      }
    }
 
+   return rc;
+}
 
+//******************************************************************************
 
+void GFSMessage::setError(tonnerre::Message& message,
+                          const string& error) {
+   GFSMessage::setKeyValue(message, KEY_ERROR, error);
+}
 
+//******************************************************************************
 
+bool GFSMessage::hasError(const tonnerre::Message& message) {
+   return GFSMessage::hasKey(message, KEY_ERROR);
+}
 
+//******************************************************************************
 
+const string& GFSMessage::getError(const tonnerre::Message& message) {
+   return GFSMessage::getKeyValue(message, KEY_ERROR);
+}
 
+//******************************************************************************
 
+void GFSMessage::setDirectory(tonnerre::Message& message,
+                              const string& directory) {
+   GFSMessage::setKeyValue(message, KEY_DIR, directory);
+}
 
+//******************************************************************************
 
+bool GFSMessage::hasDirectory(const tonnerre::Message& message) {
+   return GFSMessage::hasKey(message, KEY_DIR);
+}
 
+//******************************************************************************
 
+const string& GFSMessage::getDirectory(const tonnerre::Message& message) {
+   return GFSMessage::getKeyValue(message, KEY_DIR);
+}
 
+//******************************************************************************
 
+void GFSMessage::setFile(tonnerre::Message& message,
+                         const string& fileName) {
+   GFSMessage::setKeyValue(message, KEY_FILE, fileName);
+}
 
+//******************************************************************************
 
+bool GFSMessage::hasFile(const tonnerre::Message& message) {
+   return GFSMessage::hasKey(message, KEY_FILE);
+}
 
+//******************************************************************************
 
+const string& GFSMessage::getFile(const tonnerre::Message& message) {
+   return GFSMessage::getKeyValue(message, KEY_FILE);
+}
 
+//******************************************************************************
 
+void GFSMessage::setUniqueIdentifier(tonnerre::Message& message,
+                                     const string& uniqueIdentifier) {
+   GFSMessage::setKeyValue(message, KEY_UNIQUE_IDENTIFIER, uniqueIdentifier);
+}
 
+//******************************************************************************
 
+bool GFSMessage::hasUniqueIdentifier(const tonnerre::Message& message) {
+   return GFSMessage::hasKey(message, KEY_UNIQUE_IDENTIFIER);
+}
 
+//******************************************************************************
 
+const string& GFSMessage::getUniqueIdentifier(const tonnerre::Message& message) {
+   return GFSMessage::getKeyValue(message, KEY_UNIQUE_IDENTIFIER);
+}
+
+//******************************************************************************
+
+void GFSMessage::setOriginFileSize(tonnerre::Message& message,
+                                   unsigned long fileSize) {
+   char fileSizeString[20];
+   ::snprintf(fileSizeString, 20, "%lu", fileSize);
+   GFSMessage::setKeyValue(message, KEY_ORIGIN_FS, fileSizeString);
+}
+
+//******************************************************************************
+
+bool GFSMessage::hasOriginFileSize(tonnerre::Message& message) {
+   return GFSMessage::hasKey(message, KEY_ORIGIN_FS);
+}
+
+//******************************************************************************
+
+unsigned long GFSMessage::getOriginFileSize(tonnerre::Message& message) {
+   unsigned long fileSize = 0L;
+   const string& fileSizeString = GFSMessage::getKeyValue(message, KEY_ORIGIN_FS);
+   if (!fileSizeString.empty()) {
+      fileSize = ::strtoul(fileSizeString.c_str(), nullptr, 0);
+   }
+
+   return fileSize;
+}
+
+//******************************************************************************
+
+void GFSMessage::setStoredFileSize(tonnerre::Message& message,
+                                   unsigned long fileSize) {
+   char fileSizeString[20];
+   ::snprintf(fileSizeString, 20, "%lu", fileSize);
+   GFSMessage::setKeyValue(message, KEY_STORED_FS, fileSizeString);
+}
+
+//******************************************************************************
+
+bool GFSMessage::hasStoredFileSize(tonnerre::Message& message) {
+   return GFSMessage::hasKey(message, KEY_STORED_FS);
+}
+
+//******************************************************************************
+
+unsigned long GFSMessage::getStoredFileSize(tonnerre::Message& message) {
+   unsigned long fileSize = 0L;
+   const string& fileSizeString = GFSMessage::getKeyValue(message, KEY_STORED_FS);
+   if (!fileSizeString.empty()) {
+      fileSize = strtoul(fileSizeString.c_str(), nullptr, 0);
+   }
+
+   return fileSize;
+}
+
+//******************************************************************************
+
+void GFSMessage::setFileList(tonnerre::Message& message,
+                             const vector<string>& listFiles) {
+   if (!listFiles.empty()) {
+      const int numFileNames = listFiles.size();
+      string encodedList;
+
+      for (int i = 0; i < numFileNames; ++i) {
+         if (i > 0) {
+            encodedList += LIST_DELIMITER;
+         }
+
+         encodedList += listFiles[i];
+      }
+
+      GFSMessage::setKeyValue(message, KEY_FILE_LIST, encodedList);
+   }
+}
+
+//******************************************************************************
+
+bool GFSMessage::hasFileList(tonnerre::Message& message) {
+   return GFSMessage::hasKey(message, KEY_FILE_LIST);
+}
+
+//******************************************************************************
+
+bool GFSMessage::getFileList(tonnerre::Message& message,
+                             vector<string>& listFiles) {
+   bool success = false;
+   if (hasFileList(message)) {
+      const string& fileList = GFSMessage::getKeyValue(message, KEY_FILE_LIST);
+      if (!fileList.empty()) {
+         chaudiere::StringTokenizer st(fileList, LIST_DELIMITER);
+
+         while (st.hasMoreTokens()) {
+            listFiles.push_back(st.nextToken());
+         }
+
+         success = true;
+      }
+   }
 
    return success;
+}
 
+//******************************************************************************
 
+void GFSMessage::setDirList(tonnerre::Message& message,
+                            const vector<string>& listDirectories) {
+   if (!listDirectories.empty()) {
+      const int numEntries = listDirectories.size();
+      string encodedList;
 
+      for (int i = 0; i < numEntries; ++i) {
+         if (i > 0) {
+            encodedList += LIST_DELIMITER;
+         }
 
+         encodedList += listDirectories[i];
+      }
 
+      GFSMessage::setKeyValue(message, KEY_DIR_LIST, encodedList);
+   }
+}
 
+//******************************************************************************
 
+bool GFSMessage::hasDirList(tonnerre::Message& message) {
+   return GFSMessage::hasKey(message, KEY_DIR_LIST);
+}
 
+//******************************************************************************
 
+bool GFSMessage::getDirList(tonnerre::Message& message,
+                            vector<string>& listDirectories) {
+   bool success = false;
+   if (hasDirList(message)) {
+      const string& list = GFSMessage::getKeyValue(message, KEY_DIR_LIST);
+      if (!list.empty()) {
+         chaudiere::StringTokenizer st(list, LIST_DELIMITER);
 
+         while (st.hasMoreTokens()) {
+            listDirectories.push_back(st.nextToken());
+         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+         success = true;
+      }
+   }
 
    return success;
+}
 
+//******************************************************************************
 
+void GFSMessage::setDeviceList(tonnerre::Message& message,
+                               const vector<string>& listDevices) {
+   if (!listDevices.empty()) {
+      const int numEntries = listDevices.size();
+      string encodedList;
 
+      for (int i = 0; i < numEntries; ++i) {
+         if (i > 0) {
+            encodedList += LIST_DELIMITER;
+         }
+
+         encodedList += listDevices[i];
+      }
+
+      GFSMessage::setKeyValue(message, KEY_DEVICE_LIST, encodedList);
+   }
+}
+
+//******************************************************************************
+
+bool GFSMessage::hasDeviceList(tonnerre::Message& message) {
+   return GFSMessage::hasKey(message, KEY_DEVICE_LIST);
+}
+
+//******************************************************************************
+
+bool GFSMessage::getDeviceList(tonnerre::Message& message,
+                               vector<string>& listDevices) {
+   bool success = false;
+   if (hasDeviceList(message)) {
+      const string& list = GFSMessage::getKeyValue(message, KEY_DEVICE_LIST);
+      if (!list.empty()) {
+         chaudiere::StringTokenizer st(list, LIST_DELIMITER);
+
+         while (st.hasMoreTokens()) {
+            listDevices.push_back(st.nextToken());
+         }
+
+         success = true;
+      }
+   }
+
+   return success;
+}
+
+//******************************************************************************
 
 
